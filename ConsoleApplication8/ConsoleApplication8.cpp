4ca2d466fb4938af9fa2d248d9d5db63061d07da
Benjamin Wilkinson <wilkinsonbr@vcu.edu>
2017-10-12 15:42:02 -0400

    Corrected Line Length, and small errors.

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -12,7 +12,8 @@
 #include <string>
 using namespace std;
 
-string translateToPigLatin(string); //This function translates the words into Pig Latin.
+//This function translates the words into Pig Latin.
+string translateToPigLatin(string); 
 
 int main()
 {
@@ -20,25 +21,30 @@ int main()
 	string myLatin;  
 	char again = 'y'; //a quick variable for the while loop.
 
-	while (again == 'y' || again == 'Y') //The while loop allows the user to continue to run the program.
+	//The while loop allows the user to continue to run the program.
+	while (again == 'y' || again == 'Y') 
 {
 
 
 		cout << "Please enter a phrase you would like translated" << endl;
 		cin >> myLatin;
 
-		if (myLatin.length() > 50) //Checks if the word is too long
+		if (myLatin.length() > 48) //Checks if the word is too long
 {
 			cout << "Your word is too long. Please try again" << endl;
 }
 		//Checks if the word starts with a number
-		else if (myLatin[0] == '1' || myLatin[0] == '2' || myLatin[0] == '3' || myLatin[0] == '4' || myLatin[0] == '5' || myLatin[0] == '6' || myLatin[0] == '7' || myLatin[0] == '8' || myLatin[0] == '9' || myLatin[0] == '0')
+		else if (myLatin[0] == '1' || myLatin[0] == '2' || myLatin[0] == '3' ||
+			myLatin[0] == '4' || myLatin[0] == '5' || myLatin[0] == '6' ||
+			myLatin[0] == '7' || myLatin[0] == '8' || myLatin[0] == '9' ||
+			myLatin[0] == '0')
 {
 			cout << "Please use letters only" << endl;
 }
 		else
 {
-			myLatin = translateToPigLatin(myLatin); //Sends the word to get translated
+			//Sends the word to get translated
+			myLatin = translateToPigLatin(myLatin); 
 			cout << " Your phrase is " << endl;
 			cout << myLatin << endl;
 }
@@ -50,23 +56,31 @@ int main()
     return 0;
 }
 
-string translateToPigLatin(string latinWord) //The function that translates the string
+//The function that translates the string
+string translateToPigLatin(string latinWord) 
 {
 
 	string latin = "ay"; //added to every pig latin word.
-	char vowel = latinWord[0]; //The first letter which if not a vowel gets moved.
+	//The first letter which if not a vowel gets moved.
+	char vowel = latinWord[0]; 
 	
-	if (latinWord.length() <= 2) //Checks to see if the word is under 2 charactures
+	//Checks to see if the word is under 2 charactures
+	if (latinWord.length() <= 2) 
 {
 		latinWord = latinWord;
 }
 	//Checks to see if the first letter is a vowel. Hence the name
-	else if (vowel == 'a' || vowel == 'A' || vowel == 'e' || vowel == 'E' || vowel == 'i' || vowel == 'I' || vowel == 'o' || vowel == 'O' || vowel == 'u' || vowel == 'U')
+	else if (vowel == 'a' || vowel == 'A' || vowel == 'e' || vowel == 'E' 
+		|| vowel == 'i' || vowel == 'I' || vowel == 'o' || vowel == 'O' 
+		|| vowel == 'u' || vowel == 'U')
 {
 		latinWord = latinWord + latin;
 }
 	//Checks to see if the word is a common article or conjunction
-	else if (latinWord=="the" || latinWord == "The" || latinWord == "and" || latinWord == "And" || latinWord == "but" || latinWord == "But" || latinWord == "for" || latinWord == "For" || latinWord == "nor" || latinWord == "Nor" || latinWord == "yet" || latinWord == "Yet")
+	else if (latinWord=="the" || latinWord == "The" || latinWord == "and" 
+		|| latinWord == "And" || latinWord == "but" || latinWord == "But"
+		|| latinWord == "for" || latinWord == "For" || latinWord == "nor" 
+		|| latinWord == "Nor" || latinWord == "yet" || latinWord == "Yet")
 {
 		latinWord = latinWord;
 }
